b6019dd83463f17554fd1074d9f7e8fa9efab037
Gerald <galeg@onid.oregonstate.edu>
2015-10-28 19:54:46 -0700

    Working update on python code

--- Project2/Code/project2_Main.py
+++ Project2/Code/project2_Main.py
@@ -12,6 +12,7 @@ import time
 import random 
 import csv
 import math
+import re
 
 
 #########################
@@ -46,7 +47,7 @@ def Change_SlowPrepare(currencyValuesArray, amountToReturn):
 	return coins
 
 #Final slow algorithm
-def Change_Slow(currencyValuesArray, amountToReturn):
+def changeSlow(currencyValuesArray, amountToReturn):
 	coins = Change_SlowPrepare(currencyValuesArray, amountToReturn)
 
 	result = []
@@ -62,7 +63,7 @@ def Change_Slow(currencyValuesArray, amountToReturn):
 #Algorithm 2 - Greedy 
 ########################################
 
-def Change_Greedy(currencyValuesArray, amountToReturn):
+def changeGreedy(currencyValuesArray, amountToReturn):
 	numArray = [0] * len(currencyValuesArray)
 	total = 0
 	for i in range(len(currencyValuesArray)-1,-1,-1):
@@ -79,7 +80,7 @@ def Change_Greedy(currencyValuesArray, amountToReturn):
 #Algorithm 3 - Dynamic  
 ########################################
 
-def Change_Dynamic(currencyValuesArray, amountToReturn):
+def changeDP(currencyValuesArray, amountToReturn):
 	minArray = [0]
 	firstCoinArray = [0]
 	coin = 0
@@ -127,12 +128,20 @@ def Change_Dynamic(currencyValuesArray, amountToReturn):
 # 	RunProgram
 #	+ Get input file and starts to run the needed program
 ###############################################################
+def writeOutput(array, result, filename):
+	file = open(filename, "a") #we want to append, not truncate
+	file.write(str(array) + "\n")
+	file.write(str(result) + "\n")
+	file.close()
+	
 
-def RunProgram():
-	values = []
-	ammountToChange = []
+	
+def parser():
+	vals = []
+	change = []
 
 	inputFile = open(sys.argv[1], "r")
+	
 	while True:
 		lineValue = inputFile.readline()
 		if lineValue:
@@ -143,23 +152,57 @@ def RunProgram():
 			for i in tempArray:
 				temp2.append(int(i))
 
-			values.append(temp2)
+			vals.append(temp2)
 
 			lineValue = inputFile.readline()
-			ammountToChange.append(int(lineValue))
+			change.append(int(lineValue))
 		else:
 			break
 
 	inputFile.close()
+	return (vals, change)
 
-	for i in range(0, len(values)):
-		currencyValuesValues = sorted(values[i])
-		amount = ammountToChange[i]
-		#slowResult = Change_Slow(currencyValuesValues, amount)
-		greedyResult = Change_Greedy(currencyValuesValues, amount)
-		dpResult = Change_Dynamic(currencyValuesValues, amount)
-		print "\nGreedy value is: ", str(greedyResult), "\nDynamic Result is: ", str(dpResult)
+def funRun(funName, vals, change, fname):
+	for i in range(0, len(vals)):
+		currencyValues = sorted(vals[i])
+		amount = change[i]
+		array = funName(currencyValues, amount)
+		result = array.pop()
+		print array
+		print result
+		writeOutput(array, result, fname)
 		
+def runProgram(arg):
+	values = []
+	ammountToChange = []
+	(values, ammountToChange) = parser()
+	#this is for writing, so we only have to compile regex once
+	name = re.sub('(.txt)', '', sys.argv[1])
+	outputFile = name + "change.txt"
+	#this just resets the file at the first run
+	file = open(outputFile, "w")
+	file.close()
+	
+	if arg == 1: #slow
+		funRun(changeSlow, values, ammountToChange, outputFile)	
+			
+	elif arg == 2: #greedy
+		funRun(changeGreedy, values, ammountToChange, outputFile)
+
+	elif arg == 3: #dp
+		funRun(changeDP, values, ammountToChange, outputFile)
+		
+	elif arg == 4: #test
+		print "This doesn't do anything right now."
+		
+	elif arg == 5: #experiment aka all
+		#writeOutput("Slow:", "", outputFile)
+		#funRun(changeSlow, values, ammountToChange, outputFile)
+		writeOutput("Greedy:", "", outputFile)
+		funRun(changeGreedy, values, ammountToChange, outputFile)
+		writeOutput("\nDP:", "", outputFile)
+		funRun(changeGreedy, values, ammountToChange, outputFile)
+
 ########################################
 # END - Programs 
 ########################################
@@ -179,21 +222,16 @@ def CheckFirstArg():
 
 def CheckSecondArg():
 	if ( len(sys.argv) > 2 ):
-		if (sys.argv[2] == "-runBrute"):
-			RunProgram()
-			return True
-		elif (sys.argv[2] == "-runGreedy"):
-			RunProgram()
-			return True
-		elif (sys.argv[2] == "-runDP"):
-			RunProgram()
-			return True
-		elif (sys.argv[2] == "-runTestCorrect"):
-			RunProgram()
-			return True
-		elif (sys.argv[2] == "-runExperiment"):
-			RunProgram()
-			return True
+		if (sys.argv[2] == "-brute"):
+			return 1
+		elif (sys.argv[2] == "-greedy"):
+			return 2
+		elif (sys.argv[2] == "-dp"):
+			return 3
+		elif (sys.argv[2] == "-test"):
+			return 4
+		elif (sys.argv[2] == "-exp"):
+			return 5
 		else:
 			print "\n", str(sys.argv[2]), " is not a valid 2nd arugument."
 			return False
@@ -205,10 +243,14 @@ def CheckSecondArg():
 if __name__ == "__main__":
 	if ( len(sys.argv) > 1 ):
 		if (CheckFirstArg() == False):
-			print "\nUsage: 'python project2_Main.py input_file.txt { -runBrute | -runGreedy | -runDP | -runTestCorrect| -runExperiment }'"
-		elif (CheckSecondArg() == False):
-			print "\nUsage: 'python project2_Main.py input_file.txt { -runBrute | -runGreedy | -runDP | -runTestCorrect| -runExperiment }'"
+			print "\nUsage: 'python project2_Main.py input_file.txt { -brute | -greedy | -dp | -test| -exp }'"
 		else:
-			print "\nEverything is OK!\n"
+			arg = CheckSecondArg()
+			if (arg == False):
+				print "\nUsage: 'python project2_Main.py input_file.txt { -brute | -greedy | -dp | -test| -exp }'"
+			else:
+				print "\nValid Input, let's go!\n"
+				runProgram(arg)
 	else:
 		print "\nYou do not have enough arguments.\n"
+		print "\nUsage: 'python project2_Main.py input_file.txt { -brute | -greedy | -dp | -test| -exp }'" 
